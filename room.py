9fe9f27a9ea0f10c32471be6cf3cb9004ff2ef42
ThePhobomancer <valmir.koji@gmail.com>
2025-06-03 14:42:34 +0900

    Expanded room descriptive elements

--- room.py
+++ room.py
@@ -21,8 +21,25 @@ with open('room_objects.txt', 'r') as file_objects:
         room_objects.append(line.strip())  # strip() removes newline characters
 ##print(room_objects[0]) ##The file is read properly into the list
 
+room_features = []
+with open('room_features.txt', 'r') as file_features:
+    for line in file_features:
+        room_features.append(line.strip())  # strip() removes newline characters
+
+room_type = []
+with open('room_type.txt', 'r') as file_roomtype:
+    for line in file_roomtype:
+        room_type.append(line.strip())  # strip() removes newline characters
+
+
 ##Rooms will have a random number of objects, loot and furniture items. Each element will be placed into a room list.
 room_contents = []
+for i in range(0,random.randint(1,2)):
+    randomelement = random.choice(room_type)
+    room_contents.append(randomelement)
+for i in range(0,random.randint(1,5)):
+    randomelement = random.choice(room_features)
+    room_contents.append(randomelement)
 for i in range(0,random.randint(1,5)):
     randomelement = random.choice(room_furniture)
     room_contents.append(randomelement)
@@ -32,4 +49,7 @@ for i in range(0,random.randint(1,3)):
 for i in range(0,random.randint(1,7)):
     randomelement = random.choice(room_objects)
     room_contents.append(randomelement)
-print(room_contents)
+
+for item in room_contents:
+    print(item)
+#print(room_contents)
