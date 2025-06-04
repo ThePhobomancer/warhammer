5db0898bcc05877ab79b304a7f06fefeaee2e5a4
ThePhobomancer <valmir.koji@gmail.com>
2025-06-04 20:29:14 +0900

    Improved the output of a random basic career

--- career.py
+++ career.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
 #This picks a career and gives skill and characteristic advances
 
+import random
 #Creates a matrix and sets each line to the first career level
 career_line = [[]]
 
@@ -11,5 +12,10 @@ with open("careers_basic.txt", "r") as file:
 
 del career_line[0]
 
-for i in range(8):
-    print(career_line[i])
+
+random_career = career_line[random.randint(0, len(career_line)-1)]
+print("Career: ", random_career[0])
+for i in range(1,9):
+    print("Skill: ", random_career[i], "  +5")
+for i in range(9,13):
+    print("Talents: ", random_career[i])
