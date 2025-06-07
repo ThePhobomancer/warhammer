1be13e543996809333345f584c6ecd32d2140f9e
ThePhobomancer <valmir.koji@gmail.com>
2025-06-07 23:41:16 +0900

    Restructuring array

--- career.py
+++ career.py
@@ -14,8 +14,9 @@ del career_line[0]
 
 
 random_career = career_line[random.randint(0, len(career_line)-1)]
-print("Career: ", random_career[0])
-for i in range(1,9):
+print("Career: ", random_career[0], "Title: ", random_career[1], "Status: ", random_career[2])
+print("Career Skill: ", random_career[3], "  +10")
+for i in range(4,11):
     print("Skill: ", random_career[i], "  +5")
-for i in range(9,13):
+for i in range(11,15):
     print("Talents: ", random_career[i])
